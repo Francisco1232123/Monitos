f6ca63af02085757d89dbae31e1869551f48d128
Francisco1232123 <119644586+Francisco1232123@users.noreply.github.com>
2023-01-15 23:23:04 -0400

    Add files via upload

---
+++ mONITOS.js
@@ -0,0 +1,14 @@
+const aSmile = false;
+const bSmile = false;
+const alerta = "Verdadero; Estamos en problema";
+
+if(aSmile == false && bSmile == false || aSmile == true && bSmile == true)
+{
+
+console.log(alerta)
+}
+else 
+{
+    console.log("NO ESTAMOS EN PROBLEMAS");
+    
+}
